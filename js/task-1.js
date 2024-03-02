cd6408ef391b4da95ac3f918b22ff12bc2d04992
vladislav-chihun <vladikkosmo@gmail.com>
2024-03-02 22:01:48 +0100

    f

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categoriesList = document.querySelector("#categories");
+const categories = categoriesList.querySelectorAll("li.item");
+const elementQuantity = categories.length;
+const totalTitle = [];
+const totalList = [];
+for(let i = 0, i > )
+
+
+
+console.log(` Number of categories: ${elementQuantity}`)
+console.log(`Category:${totalTitle.join()}`)
+console.log(`Elements:${totalList.join()}`)
+
