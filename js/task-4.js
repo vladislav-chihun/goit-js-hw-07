2f5823972bfda899c20a7d8cc1648d08a421bdbc
vladislav-chihun <vladikkosmo@gmail.com>
2024-03-03 22:25:16 +0100

    f

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,15 @@
-const button = document.querySelector("button")
-console.log(button)
 const form = document.querySelector(".login-form")
-form.addEventListener("submit", event => {
-    
+form.addEventListener("submit", formFunction)
+function formFunction(event) {
     event.preventDefault();
-})
+    if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
+
+        alert('All form fields must be filled in');
+
+    } else {
+        console.log(event.currentTarget.elements.email.value.trim());
+        console.log(event.currentTarget.elements.password.value.trim());
+    }
+    event.currentTarget.reset();
+
+}
