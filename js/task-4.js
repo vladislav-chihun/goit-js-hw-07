4ed58abf9660ce5b75ed49b6ba671c76d26d4e56
vladislav-chihun <vladikkosmo@gmail.com>
2024-03-03 17:32:48 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,7 @@
-
+const button = document.querySelector("button")
+console.log(button)
+const form = document.querySelector(".login-form")
+form.addEventListener("submit", event => {
+    
+    event.preventDefault();
+})
