51e5fd17f67af05c2b2fe8456a1a206172cc778f
vladislav-chihun <vladikkosmo@gmail.com>
2024-03-03 23:06:34 +0100

    f

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const backgroundColor = document.querySelector("body")
+const textColor = document.querySelector("span")
+button.addEventListener("click", colorChange)
+function colorChange() {
+  let randomColor = getRandomHexColor();
+  backgroundColor.style.backgroundColor = `${randomColor}`
+  textColor.style.color = `${randomColor}`
+}
