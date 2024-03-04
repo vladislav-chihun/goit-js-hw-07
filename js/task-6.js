7fba08257533345b001f6063a22033614811b516
vladislav-chihun <vladikkosmo@gmail.com>
2024-03-04 12:55:40 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const divBox = document.querySelector("#boxes")
+const formContainer = document.querySelector("#controls")
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+
+
+createButton.addEventListener("click", createCollection)
+
+function createCollection() {
+  
+  const inputValue = input.value;
+  for (let i = 0; i < inputValue; i++){
+    const newElem = document.createElement("div");
+    divBox.append(newElem)
+  }
+  input.value = ""
+}
+
